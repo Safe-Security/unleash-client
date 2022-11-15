82baf62990611de3351c614fd8d17319224d6787
Aman Jain <61691832+amanjain14@users.noreply.github.com>
2022-11-15 19:05:40 +0530

    feat!: added additional parameters config (#15)
    BREAKING CHANGE: additional object added to refer the baseUrl config having a fallback value for backward compatibility
    
    Co-authored-by: Aman Jain (ECS/DEL) <aman.j@safe.security>

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,94 @@
-import { Agent } from "https";
+import * as http from "http";
+import * as https from "http";
 
-import { Strategy, initialize, isEnabled, Unleash } from "unleash-client";
+import { Strategy, initialize, Unleash } from "unleash-client";
 
+interface ParameterConfig {
+    [key: string]: {
+        valueFromMethod: <T>(arg: string) => T;
+        fallback?: string;
+    };
+}
 interface ConfigParams {
     readonly unleashServerUrl: string;
-    readonly baseUrl: string;
     readonly unleashClientApiKey: string;
+    readonly unleashAppName: string;
+    parameters: ParameterConfig;
 }
+
+const httpAgent = new http.Agent({
+    keepAlive: true,
+});
+
+const httpsAgent = new https.Agent({
+    keepAlive: true,
+});
+
+const getDefaultAgent = (url: URL) =>
+    url.protocol === "https:" ? httpsAgent : httpAgent;
+
+/**
+ * It returns the baseUrl from the parameterConfig.baseUrl.fallback property if the valueFromMethod
+ * property is not a function or if the valueFromMethod function does not return a string
+ * @param {ParameterConfig} parameterConfig - ParameterConfig
+ * @returns A function that takes a parameterConfig object and returns baseUrl.
+ */
+const getBaseUrl = (parameterConfig: ParameterConfig) => {
+    let baseUrl = parameterConfig.baseUrl.fallback;
+    const { valueFromMethod } = parameterConfig.baseUrl;
+    try {
+        if (
+            typeof valueFromMethod === "function" &&
+            typeof valueFromMethod<string>("baseUrl") === "string"
+        ) {
+            baseUrl = valueFromMethod<string>("baseUrl");
+        }
+    } catch (error) {
+        console.error(
+            "Error occurred while getting the base URL from the valueFromMethod, using fallback value now!"
+        );
+    }
+    return baseUrl as string;
+};
+
 class BaseUrlStrategy extends Strategy {
-    baseUrl: string;
-    constructor(baseUrl: string) {
+    parameterConfig: ParameterConfig;
+
+    constructor(parameters: ParameterConfig) {
         super("BaseUrl");
-        this.baseUrl = baseUrl;
+        this.parameterConfig = parameters;
     }
 
     isEnabled(parameters: { baseUrl: string }): boolean {
+        let url = getBaseUrl(this.parameterConfig);
+
         const allowedList = new Set(
             parameters.baseUrl.split(",").map(url => url.trim().toLowerCase())
         );
-        const url = this.baseUrl.toLowerCase();
-        const { hostname } = new URL(url);
-        return allowedList.has(url) || allowedList.has(hostname);
+
+        const allowedHostname = new Set<string>();
+
+        /* Trying to get the hostname from the url. */
+        [...allowedList].forEach(url => {
+            try {
+                const { hostname } = new URL(url);
+                allowedHostname.add(hostname);
+            } catch (error) {
+                error;
+            }
+        });
+
+        /* This is a check to see if the url is in the allowed list. */
+        if (url) {
+            const { hostname } = new URL(url);
+            return (
+                allowedList.has(url.trim().toLowerCase()) ||
+                allowedList.has(hostname) ||
+                allowedHostname.has(hostname)
+            );
+        }
+        console.error("No base URL can be retrieved from the parameters");
+        return false;
     }
 }
 
@@ -28,14 +96,20 @@ export const getInstance = (
     config: ConfigParams,
     refreshInterval: number = 60_000
 ): Unleash => {
-    const { unleashServerUrl, baseUrl, unleashClientApiKey } = config;
+    const {
+        unleashServerUrl,
+        unleashClientApiKey,
+        unleashAppName,
+        parameters,
+    } = config;
+
     const unleash = initialize({
         url: unleashServerUrl,
-        refreshInterval: refreshInterval,
-        appName: new URL(baseUrl).hostname,
-        strategies: [new BaseUrlStrategy(baseUrl)],
+        refreshInterval,
+        appName: unleashAppName,
+        strategies: [new BaseUrlStrategy(parameters)],
         //to leverage reuse of HTTP connections
-        httpOptions: { agent: url => new Agent({ keepAlive: true }) },
+        httpOptions: { agent: getDefaultAgent },
         customHeaders: {
             Authorization: unleashClientApiKey,
         },
